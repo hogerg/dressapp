adda3f1d2d188b9aa92afa7f36193e9c01172495
Hodány Gergő <hogerg@gmx.com>
2016-02-29 22:38:32 +0100

    Teszt Unity projekt
    Gördülő menü, sprite animáció, gomb script

---
+++ Dress App/Assets/Menu_SlideInAnimator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu_SlideInAnimator : MonoBehaviour {
+
+    Ray ray;
+    RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float posX = transform.position.x;
+        float closedX = -7.2f;
+        float openX = -5.2f;
+        float divider = 10f;
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, 1000f))
+        {
+            Vector3 dP = new Vector3((openX - posX) / divider, 0, 0);
+            //transform.position = new Vector3(-5.2f, 0, 0);
+            transform.position = transform.position + dP;
+        }
+        else
+        {
+            Vector3 dP = new Vector3((posX - closedX) / divider, 0, 0);
+            //transform.position = new Vector3(-7.2f, 0, 0);
+            transform.position = transform.position - dP;
+        }
+	}
+}
---
+++ Dress App/Assets/StickmanAnimator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickmanAnimator : MonoBehaviour {
+
+    public Sprite[] sprites;
+    public float fps;
+    private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
+        fps = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int t = (int)(Time.timeSinceLevelLoad * fps);
+        t = t % sprites.Length;
+        spriteRenderer.sprite = sprites[t];
+	}
+
+    public void setFPS (float x)
+    {
+        fps = x;
+    }
+}
