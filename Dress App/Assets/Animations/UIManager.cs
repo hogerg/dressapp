4e94f3e85c5b8d10afd0f922b51705a4a20f7dd9
Hodány Gergő <hogerg@gmx.com>
2016-03-01 16:35:25 +0100

    Menu animáció

---
+++ Dress App/Assets/Animations/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+    public void DisableBool(Animator anim)
+    {
+        anim.SetBool("isDisplayed", false);
+    }
+
+    public void EnableBool(Animator anim)
+    {
+        anim.SetBool("isDisplayed", true);
+    }
+
+    public void NegateBool(Animator anim)
+    {
+        anim.SetBool("isDisplayed", !anim.GetBool("isDisplayed"));
+    }
+
+    public void NavigateTo(int scene)
+    {
+        Application.LoadLevel(scene);
+    }
+}
