2897a21a708a5d139935e36d9b9e7d2f138e57b0
Hodány Gergő <hogerg@gmx.com>
2016-03-03 16:24:00 +0100

    semmi uj

---
+++ Dress App/Assets/Scripts/OfflineControls.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OfflineControls : MonoBehaviour {
+
+    public float speed;
+
+    void Update()
+    {
+        InputMovement();
+    }
+
+    private void InputMovement()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            GetComponent<Rigidbody2D>().MovePosition(GetComponent<Rigidbody2D>().position + Vector2.up * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            GetComponent<Rigidbody2D>().MovePosition(GetComponent<Rigidbody2D>().position - Vector2.up * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            GetComponent<Rigidbody2D>().MovePosition(GetComponent<Rigidbody2D>().position + Vector2.right * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            GetComponent<Rigidbody2D>().MovePosition(GetComponent<Rigidbody2D>().position - Vector2.right * speed * Time.deltaTime);
+    }
+}
