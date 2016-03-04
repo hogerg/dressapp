f834955aca266cf2c987955afd029a0b896b8f09
Poszi <poszy6@gmail.com>
2016-03-04 11:05:52 +0100

    negyzet átszínezés fail

--- Dress App/Assets/Scripts/color_changing_blue_button.cs
+++ Dress App/Assets/Scripts/color_changing_blue_button.cs
@@ -2,12 +2,36 @@
 using System.Collections;
 
 public class color_changing_blue_button : MonoBehaviour {
-   
-    
-	// Use this for initialization
-	void Start () {
-       
-       
+
+    private GameObject mit ;
+    private GameObject gomb1;
+    private GameObject gomb2;
+    private Color kek = Color.blue;
+    private Color piros = Color.red;
+    private Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+        mit = GameObject.Find("a_square");
+        gomb1 = GameObject.Find("Button_Blue");
+        gomb2 = GameObject.Find("Button_Red");
+        rend = mit.GetComponent<Renderer>();
+
+      /*  string shaderText =
+            "Shader \"Alpha Additive\" {" +
+         //   "Properties { _Color (\"Main Color\", Color) = (1,0,1,1) }" +
+            "SubShader {" +
+          //  "    Tags { \"Queue\" = \"Transparent\" }" +
+            "    Pass {" +
+          //  "        Blend One One ZWrite Off ColorMask RGB" +
+            "        Material { Diffuse [_Color] Ambient [_Color] }" +
+          //  "        Lighting On" +
+            "        SetTexture [_Dummy] { combine primary double, primary }" +
+            "    }" +
+            "}" +
+            "}";*/
+       // rend.material = new Material(shaderText);
+
     }
 
     // Update is called once per frame
@@ -30,8 +54,19 @@ public class color_changing_blue_button : MonoBehaviour {
         //gameObject.rend.material.color = new Color(0, 1, 0,0);
         // gameObject.GetComponent<Renderer>().material.SetColor(0, Color.blue);//
         //transform.GetComponent<Renderer>().material.color = Color.blue;//
-        transform.GetComponent<Renderer>().material.color = new Color(0, 0, 1, 1);//
+        //transform.GetComponent<Renderer>().material.color = new Color(0, 0, 1, 1);//
+
+        if (gomb1.active)
+            // mit.GetComponent<Renderer>().material.color = Color.blue;
+            // mit.GetComponent<Renderer>().material.SetColor(0, Color.blue);
+            rend.material.color = kek;
+        else if (gomb2.active)
+            // mit.GetComponent<Renderer>().material.color = Color.red;//it miafaszért nem vált ált???? miért változik a close is???? hogy lehet új materialt csináéni?
+            //mit.GetComponent<Renderer>().material.SetColor(1, Color.red);
+            rend.material.color = piros;
+        else { }
         //transform.renderer.material.color = Color.red;
     }
-    
-}
+
+
+    }
