71e7f4432ffe99c6374169540160b307ceca6de0
Poszi <poszy6@gmail.com>
2016-03-05 14:26:55 +0100

    negyzet átszínezés múkodik , negyzet mozog a babával, baba mozog, a kör scriptjével

--- Dress App/Assets/Scripts/color_changing_blue_button.cs
+++ Dress App/Assets/Scripts/color_changing_blue_button.cs
@@ -1,21 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class color_changing_blue_button : MonoBehaviour {
 
     private GameObject mit ;
-    private GameObject gomb1;
-    private GameObject gomb2;
-    private Color kek = Color.blue;
-    private Color piros = Color.red;
+    //private GameObject gomb1;
+   // private GameObject gomb2;
+    //private Color kek = Color.blue;
+    //private Color piros = Color.red;
     private Renderer rend;
+    private Color32 objectColor;
+    private GameObject baba;
 
     // Use this for initialization
     void Start () {
         mit = GameObject.Find("a_square");
-        gomb1 = GameObject.Find("Button_Blue");
-        gomb2 = GameObject.Find("Button_Red");
+        baba = GameObject.Find("baba1");
+       // gomb1 = GameObject.Find("Button_Blue");
+      //  gomb2 = GameObject.Find("Button_Red");
         rend = mit.GetComponent<Renderer>();
+
 
       /*  string shaderText =
             "Shader \"Alpha Additive\" {" +
@@ -38,6 +43,8 @@ public class color_changing_blue_button : MonoBehaviour {
     void Update()
     {
 
+        mit.transform.parent = baba.transform;
+
      /*   if (Input.GetButton("Fire1"))
         {
             gameObject.GetComponent<Renderer>().material.color = Color.blue;
@@ -46,7 +53,7 @@ public class color_changing_blue_button : MonoBehaviour {
           rend.material.SetColor("_SpecColor", Color.red);*/
         //}
     }
-    public void valami()
+  /*  public void valami()
     {
         //gameObject.GetComponent<Renderer>().material.color = Color.blue;
         //gameObject.renderer.material.SetColor("-);
@@ -60,13 +67,48 @@ public class color_changing_blue_button : MonoBehaviour {
             // mit.GetComponent<Renderer>().material.color = Color.blue;
             // mit.GetComponent<Renderer>().material.SetColor(0, Color.blue);
             rend.material.color = kek;
-        else if (gomb2.active)
-            // mit.GetComponent<Renderer>().material.color = Color.red;//it miafaszért nem vált ált???? miért változik a close is???? hogy lehet új materialt csináéni?
+        if (gomb2.active)
+            // mit.GetComponent<Renderer>().material.color = Color.red;
             //mit.GetComponent<Renderer>().material.SetColor(1, Color.red);
             rend.material.color = piros;
-        else { }
+        //else { }
         //transform.renderer.material.color = Color.red;
+       
+
+    }*/
+
+  /*  public void RecolorObject(GameObject obj)
+    {
+        Image im = GameObject.Find(obj.name).GetComponent<Image>();
+        im.color = objectColor;
     }
 
-
+    public void SetObjectColor(string hex)
+    {
+        Color c = new Color();
+        ColorUtility.TryParseHtmlString(hex, out c);
+        objectColor = c;
+        print(c.ToString());
     }
+
+    public void kekre()
+    {
+        rend.material.color = kek;
+    }
+
+    public void pirosra()
+    {
+        rend.material.color = piros;
+    }*/
+
+
+
+    public void szinezes(string hex)
+    {
+        Color c = new Color();
+
+        ColorUtility.TryParseHtmlString(hex, out c);
+        rend.material.color = c;
+    }
+
+}
---
+++ Dress App/Assets/blue_gmb_nevvissza_proba.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blue_gmb_nevvissza_proba : MonoBehaviour {
+    public Color button_color;
+    Renderer ren;
+    GameObject ez;
+
+	// Use this for initialization
+	void Start () {
+        ren = ez.GetComponent<Renderer>();
+        button_color = ren.material.color;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnClicked(GameObject button)
+    {
+        print(button.name);
+        Renderer ren = button.GetComponent<Renderer>();
+        print(ren.material.color.ToString());
+    }
+}
