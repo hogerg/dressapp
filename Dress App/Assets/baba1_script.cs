e6a7ede837cd7039eb43a5b0298c5fe363677d6c
Poszi <poszy6@gmail.com>
2016-03-10 15:02:21 +0100

    baba választás próba

---
+++ Dress App/Assets/baba1_script.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class baba1_script : MonoBehaviour {
+    GameObject baba;
+
+   public void baba1_letrehozas(string b)
+    {
+        /*GameObject baba = new GameObject();
+        baba = GameObject.Find("baba1");
+        baba = Instantiate(baba1);*/
+        baba = GameObject.Find(b);
+        Instantiate(baba);
+        baba.transform.position=new Vector3(0, 0, 0);
+    }
+
+    public void baba1_meghal()
+    {
+        Destroy(baba);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
