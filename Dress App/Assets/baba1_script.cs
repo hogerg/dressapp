06f6379082c4e23bb2f474b5e9d4af002761050a
Poszi <poszy6@gmail.com>
2016-03-10 15:44:48 +0100

    baba választás_próba2

--- Dress App/Assets/baba1_script.cs
+++ Dress App/Assets/baba1_script.cs
@@ -9,20 +9,30 @@ public class baba1_script : MonoBehaviour {
         /*GameObject baba = new GameObject();
         baba = GameObject.Find("baba1");
         baba = Instantiate(baba1);*/
+
         baba = GameObject.Find(b);
+        
         Instantiate(baba);
         baba.transform.position=new Vector3(0, 0, 0);
+
+
     }
 
-    public void baba1_meghal()
+    public void baba1_meghal(string nev)
     {
         Destroy(baba);
+       // baba = new GameObject();
+        baba = GameObject.Find(nev);
+        baba.transform.position = new Vector3(0, 0, 0);
     }
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+   /*     baba = GameObject.Find("baba1");
+        Destroy(baba);
+        baba = GameObject.Find("baba");
+        Destroy(baba);*/
+    }
 	
 	// Update is called once per frame
 	void Update () {
