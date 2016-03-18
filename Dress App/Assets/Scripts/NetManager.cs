cd0ac69b5a2364ad551985b97d2bbe7301423de5
Hodány Gergő <hogerg@gmx.com>
2016-03-18 05:23:56 +0100

    csoda

---
+++ Dress App/Assets/Scripts/NetManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetManager : NetworkManager {
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        Sprite playerSprite = playerPrefab.GetComponent<PlayerProperties>().getSkinPreset();
+        GameObject player = (GameObject)GameObject.Instantiate(playerPrefab, new Vector2(0f,-0.75f), Quaternion.identity);
+        Debug.Log(player.GetComponent<SpriteRenderer>().sprite.name);
+        player.GetComponent<SpriteRenderer>().sprite = playerSprite;
+        Debug.Log(player.GetComponent<SpriteRenderer>().sprite.name);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+}
---
+++ Dress App/Assets/Scripts/PlayerProperties.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+
+public class PlayerProperties : MonoBehaviour {
+
+    public Sprite skinPreset;
+    Sprite fullBody, top;
+    
+    void Start()
+    {
+        //Debug.Log(skinPreset.name);
+    }
+
+    public void setSkinPreset(Image skin)
+    {
+        skinPreset = skin.sprite;
+    }
+
+    public Sprite getSkinPreset()
+    {
+        return skinPreset;
+    }
+
+    public void test()
+    {
+        Debug.Log("TESTEST");
+    }
+}
